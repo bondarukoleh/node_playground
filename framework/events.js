1c1f07e0dd61d8b67a54b885f3c577669c84e013
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-01-02 23:28:11 +0200

    working with events

---
+++ framework/events.js
@@ -0,0 +1,9 @@
+const {EventEmitter} = require('events')
+
+function operation(){
+  const event = new EventEmitter()
+  process.nextTick(() => event.emit('MyEvent'))
+  return event;
+}
+
+operation().on('MyEvent', () => console.log('Event emitted'))
