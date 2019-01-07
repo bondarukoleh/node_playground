3521844e70a0f599567fc67df4d2591042f72c3f
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-01-08 00:38:25 +0200

    streams started

--- framework/stream/streamClass.js
+++ framework/stream/streamClass.js
@@ -1,33 +1,29 @@
-const {Writable} = require('stream');
+const { Writable } = require('stream');
 const http = require('http');
 const fs = require('fs');
 
 class ReadStream extends Writable {
-
-    constructor(options) {
-        super(options)
-    }
-
-    _write(chunk, encoding, cb) {
-        console.log(chunk.toString());
-        cb()
-    }
-
-    end() {
-        this.emit('thisIsTheEnd', 'Hello from end');
-    }
+  constructor(options) {
+    super(options)
+  }
+  _write(chunk, encoding, cb) {
+    console.log(chunk.toString());
+    cb()
+  }
+  end() {
+    this.emit('thisIsTheEnd', 'Hello from end');
+  }
 }
 
-module.exports = {ReadStream};
+const readStream = new ReadStream();
 
-/*{
-    const readStream = new ReadStream();
-    http.get('http://www.manning.com', (response) => {
-        console.log('GOT RESPONSE FROM HTTP');
-        response.pipe(readStream)
-    });
-    readStream.on('thisIsTheEnd', console.log);
+http.get('http://www.manning.com', (response) => {
+  response.pipe(readStream)
+});
+readStream.on('thisIsTheEnd', console.log);
+fs.createReadStream(__filename).pipe(readStream);
+readStream.on('exit', (result) => console.log(`Reading finished, result: ${result}`));
 
-    fs.createReadStream(__filename).pipe(readStream);
-    readStream.on('exit', (result) => console.log(`Reading finished, result: ${result}`));
-}*/
+/*Streams can be readable or writable, and are implemented with instances of
+EventEmitter—see chapter 4 for more on events. Streams provide the means for creating
+data flows between objects, and can be composed with LEGO-like modularity. */
