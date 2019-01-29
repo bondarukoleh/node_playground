4cbc34f307270b6d854b6d4c9940971f52ede1db
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-01-30 00:04:02 +0200

    http continued

--- nodeAbilities/net/http.js
+++ nodeAbilities/net/http.js
@@ -8,6 +8,12 @@ const server = http.createServer((req, res) => {
     res.write('Yeah you got it!');
     return res.end();
   };
+  if(req.url === '/error') {
+    // res.writeHead(500, { 'Error-Message': http.STATUS_CODES[500] });
+    res.writeHead(500);
+    res.write(http.STATUS_CODES[500]);
+    return res.end();
+  };
   console.log('Got request');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello fella!');
@@ -22,36 +28,37 @@ server.on('connection', (socket) => {
    server.close(_ => console.log('SHUTTED'))) - Shutting server vilently,
     better destroy all socket connections before*/
   socket.on('close', _ => console.log(`Connection down`),
-   server.unref(_ => console.log('SHUTTED'))) /* Waits untill connections out */
+   server.unref(_ => console.log('SHUTTED'))) /* Waits untill all sockets out */
 });
 server.listen(port);
 
 setTimeout(() => {
   http.get('http://localhost:3000/special', (response) => {
     const { statusCode } = response;
+    console.log(statusCode);
     response.on('data', (data) => {
       console.log('Have response: %s', data);
     })
     if (statusCode === 500) {
-      console.log('SERVER ERROR:', response.headers.message);
+      console.log('SERVER ERROR:', response.headers);
     }
     response.on('error', (err) => {
-      throw new Error('WENT WRRONG:', err)
+      throw new Error('WENT WRONG:', err)
     })
   })
 }, 1000)
 
 setTimeout(() => {
-  http.get('http://localhost:3000/special', (response) => {
+  http.get('http://localhost:3000/error', (response) => {
     const { statusCode } = response;
     response.on('data', (data) => {
       console.log('Have response: %s', data);
     })
     if (statusCode === 500) {
-      console.log('SERVER ERROR:', response.headers.message);
+      console.log('SERVER ERROR:', response.headers);
     }
     response.on('error', (err) => {
       throw new Error('WENT WRRONG:', err)
     })
   })
-}, 7000)
+}, 4000)
