5399c6ccb251df004f50b4ce3c13d66483816c41
Oleh Bondaruk <olebon@como.com>
2019-02-15 11:35:26 +0200

    test fixes

--- specs/specsToCheckLibraries/checkTestLibraries.spec.js
+++ specs/specsToCheckLibraries/checkTestLibraries.spec.js
@@ -2,33 +2,33 @@ const {expect} = require('chai');
 const assertNode = require('assert');
 const {assert} = require('chai')
 
-describe('Describe 1', () => {
-    it('D1 it 1', async () => {
+describe('First describe', () => {
+    it('D1 first it', async () => {
         expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`);
         assertNode(true, true, `True should be true. Don't be a shmak, be true`)
     });
-    it('D1 it 2 super first name', async () => {
+    it('D1 second it', async () => {
         expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`);
-        assert.strictEqual([1, 2], [1, 2], `Trying to compare arrays`)
+        assert.deepEqual([1, 2], [1, 2], `Trying to compare arrays`)
     });
     it.skip('D1 it 3 skipped', async () => {
         expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`)
     });
 });
 
-describe('Describe 2', () => {
-    it('D2 it 1', async () => {
+describe('Second describe', () => {
+    it('D2 first it', async () => {
         expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`)
     });
-    it('D2 it 2 super second name', async () => {
+    it('D2 second it', async () => {
         expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`)
     });
 
     describe('Inner describe', () => {
-        it('Inner it 1', async () => {
-            expect(true).to.eq(false, `True should be true. Don't be a shmak, be true`)
+        it('Inner describe first it', async () => {
+            expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`)
         });
-        it('Inner it 2 super inner name', async () => {
+        it('Inner describe second it', async () => {
             expect(true).to.eq(true, `True should be true. Don't be a shmak, be true`)
         });
     });
