84b179a9fd08733e03b27dc5f1d9a204266fc679
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-02-27 23:50:46 +0200

    decorators refactores

--- someMessyStuff/decorator.js
+++ someMessyStuff/decorator.js
@@ -1,59 +1,59 @@
 class ShouldBeDecorated {
   constructor() {
+    decorate(this, ['asyncCall', 'syncCall'])
+    this.instanseValue = 'some instanse value'
   }
 
-  async asyncCaller() {
-    return new Promise((res) => res(true))
+  async asyncCall(...args) {
+    console.log(`Hello from asyncCall, got args ${args}`);
+    console.log(`instanse value - ${this.instanseValue}`);
+    return new Promise((res) => res(args))
   }
 
-  syncCaller(...args) {
-    return args
+  syncCall(arg1, arg2) {
+    console.log(`Hello from syncCall, got args ${arguments[0]} ${arguments[1]}`);
+    console.log(`instanse value - ${this.instanseValue}`);
+    return arguments
   }
 }
 
-function decorate(classItem, functions) {
-
-  for(const fun of functions) {
-    const originalFn = classItem.prototype[fun]
+function decorate(contextToDecorate, functionsTodecorate) {
+  for(const functionName of functionsTodecorate) {
+    const originalFunction = contextToDecorate[functionName]
     const returnStringType = (arg) => Object.prototype.toString.call(arg)
-    if(originalFn.then || returnStringType(originalFn) === '[object AsyncFunction]') {
-      classItem.prototype[fun] = async function(...args) {
-        // allure step start
-        console.log('HERE')
-        let result
+    if(originalFunction.then || returnStringType(originalFunction) === '[object AsyncFunction]') {
+      contextToDecorate[functionName] = async function(...args) {
+        console.log('allure step start')
+        let result = null;
         try {
-          result = await originalFn(...args)
-          // allure step end success
-          console.log('HERE1')
+          result = await originalFunction.call(that, ...args)
+          console.log('allure step end success')
+          console.log(`Log success function "${functionName}" with arguments: ${args}.`)
           return result
         } catch(error) {
-          // allure step end failed
+          console.log('allure step end failed')
+          console.log(`Log failed function "${functionName}" with arguments: ${args}.`)
           throw error
         }
       }
     } else {
-      classItem.prototype[fun] = function(...args) {
-        // allure step start
-        let result
+      contextToDecorate[functionName] = function(...args) {
+        console.log('allure step start')
+        let result = null;
         try {
-          console.log('HERE')
-          result = originalFn(...args)
-          // allure step end success
+          result = originalFunction.call(that, ...args)
+          console.log('allure step end success')
+          console.log(`Log success function ${functionName} and arguments: ${args}.`)
           return result
         } catch(error) {
-          console.log('HERE1')
-          // allure step end failed
+          console.log('allure step end failed')
+          console.log(`Log failed function ${functionName} and arguments: ${args}.`)
           throw error
         }
       }
     }
   }
 }
-decorate(ShouldBeDecorated, ['asyncCaller', 'syncCaller'])
-
-// const cl = new ShouldBeDecorated()
-
-// console.log(cl.syncCaller('dsada'))
-const o = ShouldBeDecorated.prototype.asyncCaller
-
-console.log(o.constructor.name);
+const decoratedInstanse = new ShouldBeDecorated()
+decoratedInstanse.asyncCall('asyncArg1', 'asyncArg2').then(console.log)
+console.log(decoratedInstanse.syncCall('syncArg1', 'syncArg2'))
--- someMessyStuff/decoratorMoreClear.js
+++ someMessyStuff/decoratorMoreClear.js
@@ -1,36 +1,38 @@
 const asyncStep = async (name, stepFunction, args) => {
   try {
-    console.log(`Start step`);
-    const functionResult = await stepFunction(...args);
-    console.log(`Start end`);
-    return functionResult;
-  } catch (e) {
-    console.log(`Start end`);
-    throw e;
+    result = await stepFunction(...args)
+    console.log('allure step end success')
+    console.log(`Log success function "${name}" with arguments: ${args}.`)
+    return result
+  } catch(error) {
+    console.log('allure step end failed')
+    console.log(`Log failed function "${name}" with arguments: ${args}.`)
+    throw error
   }
 };
 
 const syncStep = (name, stepFunction, args) => {
   try {
-    console.log(`Start step`);
-    const functionResult = stepFunction(...args);
-    console.log(`Start end`);
-    return functionResult;
-  } catch (e) {
-    console.log(`Start end`);
-    throw e;
+    result = stepFunction(...args)
+    console.log('allure step end success')
+    console.log(`Log success function ${name} and arguments: ${args}.`)
+    return result
+  } catch(error) {
+    console.log('allure step end failed')
+    console.log(`Log failed function ${name} and arguments: ${args}.`)
+    throw error
   }
 };
 
-const stepDecorator = (classFunction, functionList) => {
+const stepDecorator = (contextToDecorate, functionList) => {
   for (const functionName of functionList) {
-    const originalFunction = classFunction.prototype[functionName];
+    const originalFunction = contextToDecorate[functionName].bind(contextToDecorate);
     if (originalFunction.then || originalFunction.constructor.name === 'AsyncFunction') {
-      classFunction.prototype[functionName] = async function (...args) {
+      contextToDecorate[functionName] = async function (...args) {
         return asyncStep(functionName, originalFunction, args);
       };
     } else {
-      classFunction.prototype[functionName] = function (...args) {
+      contextToDecorate[functionName] = function (...args) {
         return syncStep(functionName, originalFunction, args);
       };
     }
@@ -39,20 +41,23 @@ const stepDecorator = (classFunction, functionList) => {
 
 class ShouldBeDecorated {
   constructor() {
+    stepDecorator(this, ['asyncCall', 'syncCall'])
+    this.instanseValue = 'some instanse value'
   }
 
-  async asyncCaller() {
-    return new Promise((res) => res(true))
+  async asyncCall(...args) {
+    console.log(`Hello from asyncCall, got args ${args}`);
+    console.log(`instanse value - ${this.instanseValue}`);
+    return new Promise((res) => res(args))
   }
 
-  syncCaller(args, dfd) {
-    console.log('args');
-    console.log(args, dfd);
-    return args
+  syncCall(arg1, arg2) {
+    console.log(`Hello from syncCall, got args ${arguments[0]} ${arguments[1]}`);
+    console.log(`instanse value - ${this.instanseValue}`);
+    return arguments
   }
 }
 
-stepDecorator(ShouldBeDecorated, ['asyncCaller', 'syncCaller'])
-console.log(ShouldBeDecorated);
-const decoratedInstance = new ShouldBeDecorated()
-decoratedInstance.syncCaller(111, 32354)
+const decoratedInstanse = new ShouldBeDecorated()
+decoratedInstanse.asyncCall('asyncArg1', 'asyncArg2').then(console.log)
+console.log(decoratedInstanse.syncCall('syncArg1', 'syncArg2'))
