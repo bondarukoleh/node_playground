e92096696e6763cebd66b1294cf998c2f57cbedb
Oleh <bondaruk9000@gmail.com>
2019-06-20 17:27:23 +0300

    attachment to smtp sender added

--- nodeCore/smtp/testSmtp.js
+++ nodeCore/smtp/testSmtp.js
@@ -1,15 +1,15 @@
 const nodemailer = require('nodemailer');
 
-function sendSMTPMessage(){
+function sendSMTPMessage() {
   nodemailer.createTestAccount((err, account) => {
     console.log(account)
     if (err) {
       console.error('Failed to create a testing account. ' + err.message);
       return process.exit(1);
     }
-  
+
     console.log('Credentials obtained, sending message...');
-  
+
     // Create a SMTP transporter object
     let transporter = nodemailer.createTransport({
       host: account.smtp.host,
@@ -20,22 +20,28 @@ function sendSMTPMessage(){
         pass: account.pass
       }
     });
-  
+
     // Message object
     let message = {
       from: 'Sender Name <sender@example.com>',
       to: 'Recipient <recipient@example.com>',
       subject: 'Nodemailer is unicode friendly ✔',
       text: 'Hello to myself!',
+      attachments: [
+        {
+          filename: 'a.txt',
+          content: Buffer.from('SOME TEXT UUUU')
+        }
+      ],
       html: '<p><b>Hello</b> to myself!</p>'
     };
-  
+
     transporter.sendMail(message, (err, info) => {
       if (err) {
         console.log('Error occurred. ' + err.message);
         return process.exit(1);
       }
-  
+
       console.log('Message sent: %s', info.messageId);
       // Preview only available when sending through an Ethereal account
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
@@ -43,7 +49,7 @@ function sendSMTPMessage(){
   });
 }
 
-function sendMessageToMailCatcher(){
+function sendMessageToMailCatcher() {
   const transporter = nodemailer.createTransport({
     host: 'localhost', // you need to start mail catcher locally
     port: 1025,
@@ -58,7 +64,7 @@ function sendMessageToMailCatcher(){
   };
 
   transporter.sendMail(mail, function (err, mailResult) {
-    if(err){console.log(err)}
+    if (err) { console.log(err) }
     console.log(mailResult)
   });
 }
